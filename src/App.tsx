4542419eedf1a92ff7c4053b14328069151eb2dd
khsacc <hero.key629522@gmail.com>
2020-10-09 00:48:29 +0900

    created form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,62 @@
 import React from 'react';
-function App() {
+import { makeStyles } from '@material-ui/core';
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, TextField } from '@material-ui/core'
+import { courses2A, courses3S } from './courses'
+
+const grades = ['A+', 'A', 'B', 'C', 'Fail', 'Absent']
+
+const useStyles = makeStyles(() => {
+  return {
+    wrapper: {
+      background: '#ffefe8',
+      minHeight: '100vh',
+      padding: 10
+    },
+    textInput: {
+      padding: '0 10px 10px 0'
+    },
+    eachInputWrap: {
+      margin: '15px 0'
+    }
+  }
+})
+
+const App = () => {
+  const classes = useStyles()
   return (
-    <div className="App">
+    <div className={classes.wrapper}>
+      <h1>Lab selection score calculator</h1>
+      <TextField required label="Student ID Number" variant="outlined" className={classes.textInput} />
+      <TextField required label="Your Name (full)" variant="outlined" className={classes.textInput} />
+      <br />
+      <h2>2A semester</h2>
+      <FormControl component="fieldset">
+        {courses2A.map((course, idx) => (
+          <div key={idx} className={classes.eachInputWrap}>
+            <FormLabel component="legend">{course.name}</FormLabel>
+            <RadioGroup aria-label={course.name} name={course.name} row>
+              {
+                grades.map(grade => <FormControlLabel key={grade} value={grade} control={<Radio color="default" />} label={grade} />)
+              }
+            </RadioGroup>
+          </div>
+        ))}
+      </FormControl>
+      <h2>3S semester</h2>
+      <FormControl component="fieldset">
+        {courses3S.map((course, idx) => (
+          <div key={idx} className={classes.eachInputWrap}>
+            <FormLabel component="legend">{course.name}</FormLabel>
+            <RadioGroup aria-label={course.name} name={course.name} row aria-required>
+              {
+                grades.map(grade => <FormControlLabel key={grade} value={grade} control={<Radio color="default" />} label={grade} />)
+              }
+            </RadioGroup>
+          </div>
+        ))}
+      </FormControl>
     </div>
+    
   );
 }
 
---
+++ src/courses.ts
@@ -0,0 +1,54 @@
+export interface course { name: string; credit: number }
+export const courses2A: course[] = [
+  {
+    name: 'Quantum Chemisty I',
+    credit: 2,
+  },
+  {
+    name: 'Analytical Chemisty I',
+    credit: 2,
+  },
+  {
+    name: 'Organic Chemisty I',
+    credit: 4,
+  },
+  {
+    name: 'Inorganic Chemisty I',
+    credit: 2,
+  },
+  {
+    name: 'Chemical Thermodynamics I',
+    credit: 2,
+  }
+]
+
+export const courses3S: course[] = [
+  {
+    name: 'Organic Chemistry II',
+    credit: 4,
+  },
+  {
+    name: 'Chemical Thermodynamics II',
+    credit: 2,
+  },
+  {
+    name: 'Solid State Chemistry',
+    credit: 2,
+  },
+  {
+    name: 'Radiochemistry',
+    credit: 1,
+  },
+  {
+    name: 'Structural Chemisty',
+    credit: 2,
+  },
+  {
+    name: 'Inorganic Chemisty II',
+    credit: 2,
+  },
+  {
+    name: 'Organic Chemisty III',
+    credit: 2,
+  },
+]
