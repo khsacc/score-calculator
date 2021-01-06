c2441e7778554bd9e1c3f424f56a4e7e2e397370
khsacc <hero.key629522@gmail.com>
2021-01-06 11:48:46 +0900

    temporaly disabled

--- src/App.tsx
+++ src/App.tsx
@@ -268,8 +268,8 @@ const App = () => {
           onClick={() => {
             generatePDF()
           }}
-          disabled={!isReady}
-          // disabled={true}
+          // disabled={!isReady}
+          disabled={true}
         >
           {generated ? 'Re-generate PDF' : 'Generate PDF'}
       </Button>
